364b5961de6d9d3b106c1ea113820685a38059c2
franky-100977 <80473323+franky-100977@users.noreply.github.com>
2025-08-13 12:00:29 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~dq9E2brT#H2SuwHJjR4aTceknfCyOsmTplnOPuDqj642gFP5u1wA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "237695077107", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
